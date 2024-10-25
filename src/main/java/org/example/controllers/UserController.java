9ad07897cd4ef1e18b0f1b392e0c940a27bfdf69
khalidwaheli <oukhakhalid@gmail.com>
2024-10-25 12:53:49 +0100

    added controller layer for user entity

---
+++ src/main/java/org/example/controllers/UserController.java
@@ -0,0 +1,28 @@
+package org.example.controllers;
+
+import org.example.entities.User;
+import org.example.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+    
+}
