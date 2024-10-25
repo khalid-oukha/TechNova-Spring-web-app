99c26995012c66d850cf3f7e146734ea8dd9162c
khalidwaheli <oukhakhalid@gmail.com>
2024-10-25 12:50:04 +0100

    added service layer for user

---
+++ src/main/java/org/example/services/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package org.example.services;
+
+import org.example.entities.User;
+import org.example.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
